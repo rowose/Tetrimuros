44196536a60da5c1c00480cb93f214832b03d6b8
Julien <ankadre@hotmail.fr>
2020-01-09 15:00:12 +0100

    First push, controls done

---
+++ rotating_thing/Assets/Scripts/Spawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [Header ("Parameters")]
+    [SerializeField] Wall[] walls;
+    [SerializeField] float spawnDelay = 2f;
+    [SerializeField] float baseSpeed = 2f;
+
+    float lastWall = 0f;
+
+    private void Start() {
+        InvokeRepeating("SpawnWall", 0, spawnDelay);
+    }
+
+    private void Update() {
+        
+    }
+
+    private void SpawnWall()
+    {
+        InstantiateWall().SetSpeed(baseSpeed);
+    }
+
+    private Wall InstantiateWall()
+    {
+        Wall wall = null;
+		var i = Random.Range(0, walls.Length);
+
+		if (i != lastWall)
+		{
+			wall = Instantiate(walls[i], transform.position, transform.rotation);
+			lastWall = i;
+		}
+        else
+            return InstantiateWall();
+
+        return wall;
+    }
+}
---
+++ rotating_thing/Assets/Scripts/Wall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    [Header ("Parameters")]
+    [SerializeField] Vector3 direction;
+
+    float speed = 0f;
+
+    void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+
+    public void SetSpeed(float value)
+    {
+        speed = value;
+    }
+}
